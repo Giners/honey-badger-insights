1032b45e742bfc7f142bbaaa077c08bc36857060
Chris Austin <caustin.adwan@gmail.com>
2018-04-23 11:06:49 +0000

    chore(setup): setup basic server/API service
    This commit lays down the foundation for a server that uses an API
    service to respond to HTTP requests. In doing so we have taken the
    initial steps to start compiling the server code by adding using Babel
    to transpile our code.
    
    Partially satisfies: #1
    	new file:   .babelrc
    	new file:   .gitignore
    	new file:   package.json
    	new file:   src/server/config.js
    	new file:   src/server/honeyBadgerApp.js
    	new file:   src/server/server.js
    	new file:   src/server/services/apiService.js
    	new file:   yarn.lock

---
+++ src/server/config.js
@@ -0,0 +1,8 @@
+const config = {
+  // Config for the app/server
+  app: {
+    port: process.env.port || 3000,
+  },
+}
+
+export default config
---
+++ src/server/services/apiService.js
@@ -0,0 +1,15 @@
+/*
+ * Express middleware for an API service.
+ *
+ * @param {object} req - Represents the HTTP request making a request to the API service. Has
+ * properties for the request query string, parameters, body, HTTP headers, and more. To learn more
+ * about it you can visit this documentation: https://expressjs.com/en/4x/api.html#req
+ * @param {object} res - Represents the HTPT response that is to contain the results of the API call
+ * invoked by the HTTP request. To learn more about it you can visit the documentation:
+ * https://expressjs.com/en/4x/api.html#res
+ */
+const apiService = (req, res) => {
+  res.status(200).json({ status: 'API service is handling requests' }).end()
+}
+
+export default apiService
