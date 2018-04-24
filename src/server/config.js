29e30566b28aed16a2ea6c3a480e8185d558646d
Chris Austin <caustin.adwan@gmail.com>
2018-04-24 16:15:54 +0000

    feat(gql): implement initial 'honeyBadgers' query
    This commit implements our first query named 'honeyBadgers'. The query
    allows client consumers to query for the top intruders of a honeypot. As
    part of this commit we also setup our schema since it wasn't yet
    implemented.
    
    Closes #2

--- src/server/config.js
+++ src/server/config.js
@@ -3,6 +3,13 @@ const config = {
   app: {
     port: process.env.port || 3000,
   },
+  // Service credentials
+  serviceCreds: {
+    honeyDB: {
+      id: process.env.HONEYDB_API_AUTH_ID || 'No API auth ID found!',
+      key: process.env.HONEYDB_API_AUTH_KEY || 'No API auth key found!',
+    },
+  },
 }
 
 export default config
