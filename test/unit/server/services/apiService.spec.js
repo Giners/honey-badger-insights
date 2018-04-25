c981911b182e0b3516dcbc503450e0ba6e4e36a6
Chris Austin <caustin.adwan@gmail.com>
2018-04-25 09:53:22 +0000

    feat(gql-svc): setup GraphQL API endpoint/service
    This commit creates a GraphQL API endpoint/service. It is
    consumed/exposed as Express middleware.
    
    In addition we have added a lot of packages as 'dependencies' that
    aren't really dependencies at the moment. I think this is okay as we
    will be using them when we begin clientside work soon.
    
    Closes #5

--- test/unit/server/services/apiService.spec.js
+++ test/unit/server/services/apiService.spec.js
@@ -11,45 +11,65 @@
 /* eslint no-unused-expressions: 0, chai-friendly/no-unused-expressions: 2 */
 
 // Supporting test libs
-import request from 'supertest'
 import { expect } from 'chai'
 
-// Supporting lips
+// Supporting libs
 import express from 'express'
+import { ApolloClient } from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import gql from 'graphql-tag'
 
 // Code under test
 import apiService from './../../../../src/server/services/apiService'
 
-describe('HoneyBadgerApp service test: apiService', function() {
-  // Our HTTP request interface that will be bound to our running app
-  let httpRequest = null
+// Supporting app code
+import config from './../../../../src/server/config'
 
-  before('Setup server', function() {
+describe('HoneyBadgerApp service test: apiService', function() {
+  // Reference to our server we use to test our API service. Needs to be cleaned up when the tests
+  // end.
+  let testServer = null
+
+  before('Setup test server with GraphQL API service/endpoint', function(done) {
     const app = express()
     app.use(apiService)
 
-    httpRequest = request(app)
+    testServer = app.listen(config.app.port, () => {
+      expect(testServer).to.not.be.null
 
-    expect(httpRequest).to.exist
+      done()
+    })
   })
 
-  it('200 OK', function(done) {
-    httpRequest.get('').expect(200, done)
+  after('Cleanup test server with GraphQL API service/endpoint', function(
+    done,
+  ) {
+    testServer.close(() => {
+      done()
+    })
   })
 
-  it('Content-Type: application/json', function(done) {
-    httpRequest
-      .get('')
-      .expect('Content-Type', 'application/json; charset=utf-8', done)
-  })
+  it('Serves GraphQL requests', function() {
+    // Increase the timeout in our unit test as we just use an arbitrary query from our schema
+    // ('honeyBadgers') to verify that our API endpoint is setup and serving requests yet
+    // unfortunately it takes a while as its communicating with HoneyDB. If we implement a query in
+    // the future that doesn't take as long then we can remove this timeout.
+    this.timeout(5000)
 
-  it('Body contains correct content', function(done) {
-    httpRequest
-      .get('')
-      .expect((res) => {
-        expect(res.body.status).to.exist
-        expect(res.body.status).to.equal('API service is handling requests')
-      })
-      .end(done)
+    const client = new ApolloClient({
+      link: new HttpLink({ uri: `http://localhost:${config.app.port}` }),
+      cache: new InMemoryCache(),
+    })
+
+    return client.query({
+      query: gql`
+        {
+          honeyBadgers {
+            ipAddress
+          }
+        }
+      `,
+    })
   })
 })
